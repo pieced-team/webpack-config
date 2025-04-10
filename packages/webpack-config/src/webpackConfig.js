26b97ca919e9f4d4879f0592dc2dc7445d06fbbb
mingdev <mingdev@163.com>
2025-04-10 16:33:45 +0800

    feat(webpack-config): 如果配置是函数时将已合并的配置导出

--- packages/webpack-config/src/webpackConfig.js
+++ packages/webpack-config/src/webpackConfig.js
@@ -3,11 +3,21 @@ const devConfig = require('./devConfig');
 const prodConfig = require('./prodConfig');
 
 module.exports = (...config) => (env, args) => {
-  const mergeConfig = config.map((curCfg) => (typeof curCfg === 'function' ? curCfg(env, args) : curCfg));
   const nodeEnv = process.env.NODE_ENV || args.mode;
+  const isDev = nodeEnv === 'development' || env.WEBPACK_SERVE;
+  const isProd = nodeEnv === 'production' || env.WEBPACK_BUILD;
 
-  if (nodeEnv === 'development') return merge(devConfig, ...mergeConfig);
-  if (nodeEnv === 'production') return merge(prodConfig, ...mergeConfig);
+  const mergeFn = (baseConfig) => config.reduce(
+    (preConfig, curConfig) => {
+      if (typeof curConfig === 'function') return merge(preConfig, curConfig(env, args, preConfig));
+
+      return merge(preConfig, curConfig);
+    },
+    baseConfig,
+  );
+
+  if (isDev) return mergeFn(devConfig);
+  if (isProd) return mergeFn(prodConfig);
 
   throw new Error('No matching configuration was found!');
 };
